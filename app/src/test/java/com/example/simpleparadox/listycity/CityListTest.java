e33a1fd757a0c661996ae5c51b9eb8658f0382c3
XZPshaw <zepeng@ualberta.ca>
2020-11-01 20:19:51 -0700

    a new failed test added

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -32,6 +32,7 @@ class CityListTest {
     @Test
     void testShouldFail(){
         CityList cityList = mockCityList();
+        cityList.add(new City("Edmonton","Alberta"));
         assertEquals(2, cityList.countCities());
 
     }
